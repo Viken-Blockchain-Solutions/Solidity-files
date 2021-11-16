969f23dfeb84b5155e4a6993072d0398d2adc0e3
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2021-11-16 22:36:44 +0100

    Add boilerplate contract

---
+++ Templates/Smart-contract-template/boilerplate.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Boilerplate {
+   
+   // Give your contract a name and save file to local project folder.
+
+
+
+
+}
