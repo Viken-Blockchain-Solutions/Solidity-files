fa6d20b199ebb93a7c86934b222bdea69d474e0d
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2021-12-31 22:32:41 +0100

    test: add unit-tests for stakingContract.sol

---
+++ smart-contracts/test/stakingContract.test.js
@@ -0,0 +1,49 @@
+const { expect } = require("chai");
+// `describe` receives the name of a section of your test suite, and a callback.
+// The callback must define the tests of that section. This callback can't be
+// an async function.
+describe("Staking contract", function () {
+  // Mocha has four functions that let you hook into the the test runner's
+  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
+
+  // They're very useful to setup the environment for tests, and to clean it
+  // up after they run.
+
+  // A common pattern is to declare some variables, and assign them in the
+  // `before` and `beforeEach` callbacks.
+
+  let Contract;
+  let instance;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  before(async function () {
+    // Get the ContractFactory and Signers here.
+    Contract = await ethers.getContractFactory("StakingContract");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // for it to be deployed(), which happens once its transaction has been
+    // mined.
+    instance = await Contract.deploy();
+  });
+  // You can nest describe calls to create subsections.
+  describe("Deployment", function () {
+    // `it` is another Mocha function. This is the one you use to define your
+    // tests. It receives the test name, and a callback function.
+    // If the callback function is async, Mocha will `await` it.
+    it("Should set the right owner", async function () {
+      // Expect receives a value, and wraps it in an Assertion object. These
+      // objects have a lot of utility methods to assert values.
+
+      // This test expects the owner variable stored in the contract to be equal
+      // to our Signer's owner.
+      expect(await instance.owner()).to.equal(owner.address);
+    });
+  });
+
+})
