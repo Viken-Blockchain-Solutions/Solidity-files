b289a7787ab3417907cd6a981f4215807ec45115
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-01-03 08:42:22 +0100

    wip: refactor

--- smart-contracts/contracts/StakingContract.sol
+++ smart-contracts/contracts/StakingContract.sol
@@ -5,35 +5,58 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./testERC20.sol";
+import "./SafeERC20.sol";
 
+/** 
+ * @notice This contract with let an user deposit funds into a Vault with a given lockperiod.
+ *          After the lockperiod, the user can withdraw the funds and the accumulated rewards 
+ *          back to their wallet.
+ */
 contract StakingContract is Ownable {
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
-    testERC20 stakeToken;
+    IERC20 public stakeToken;
 
-    /**
-    * @notice Array of addresses to keep track of all the stakeholders.
-    */
-    address[] internal stakeholders;
+    mapping(address => Vault) internal VaultsMapping;
 
-    /**
-    * @notice The stakes for each stakeholder.
-    */
-    mapping(address => uint256) internal stakes;
+    event StakingToken(address stakeToken);
 
-    /**
-    * @notice The accumulated rewards for each stakeholder.
-    */
-    mapping(address => uint256) internal rewards;
+    event AddedToVault(uint256 staked, address stakeholder);
 
-    event CurrentlyStaking(address stakeToken);
+    event ReceiveReverted(uint256 value);
 
     /**
-    * @notice receive function reverts and returns the funds to the sender.
-    */ 
+     * @notice Vault to hold the stakeholder data.
+     * @param isStaking Bool.
+     * @param tokenAddress Address of staked ERC20.
+     * @param staked The amount of this stakeholder id.   
+     * @param reward The reward amount of this stakeholder id.   
+     */
+    struct Vault {
+        bool isStaking;
+        address tokenAddress;
+        uint256 amount;
+        uint256 reward;
+    }
+
+    /**
+     * @notice receive function reverts and returns the funds to the sender.
+     */ 
     receive() external payable {
         revert("not payable receive");
+
+        emit ReceiveReverted(msg.value);
+    }
+
+    /**
+     * @notice Authorizes this StakingContract to spend the msg.senders token (ERC20).
+     * @param _erc20Address Address of an ERC20 used as stakingToken.
+     */
+    function approveToSpendToken(address _erc20Address) external {
+        IERC20 erc20 = IERC20(_erc20Address);
+        uint256 max = 2**256 - 1;
+        erc20.safeApprove(address(this), max);
     }
 
     /**
@@ -41,168 +64,43 @@ contract StakingContract is Ownable {
      * @param _tokenAddress The TokenAddress to stake.
      */
     function setToken(address _tokenAddress) external onlyOwner {
-        stakeToken = testERC20(_tokenAddress);
+        stakeToken = IERC20(_tokenAddress);
 
-        emit CurrentlyStaking(_tokenAddress);
+        emit StakingToken(_tokenAddress);
     }
 
-   /**
-    * @notice Returns the name of the token staked.
-    */
-    function stakedERC20() public view virtual returns (string memory) {
-        return stakeToken.name();
+    /**
+     * @notice Add stakeholder and stake amount to the vault.
+     * @param _stake Amount to be staked.
+     * @dev Checks allowance and calls safeAppprove() if needed.
+     */
+    function addToVault(uint256 _stake) external returns (bool) {
+        if (_stake <= 0 && msg.sender == address(0)) revert("Zero values.");
+        
+        uint256 amount = _stake;
+        _stake = 0;
+        
+        if (stakeToken.allowance(address(msg.sender), address(this)) < amount) {
+            (stakeToken).safeApprove(address(this), amount);
+        } revert("Not authorized");
+        
+        VaultsMapping[msg.sender] = Vault(
+            true,
+            address(stakeToken),
+            amount,
+            0
+        );
+
+        require(address(stakeToken).safeTranferFrom(
+            stakeToken,
+            address(msg.sender), 
+            address(this), 
+            amount
+            ), "AddToVault failed"
+        );
+
+        emit AddedToVault(amount, msg.sender);
+
+        return true;
     }
-
-   /**
-    * @notice A method to check if an address is a stakeholder.
-    * @param _address The address to verify.
-    * @return bool, uint256 Whether the address is a stakeholder,
-    * and if so its position in the stakeholders array.
-    */
-   function isStakeholder(address _address) public view returns (bool, uint256) {
-       for (uint256 s = 0; s < stakeholders.length; s += 1){
-           if (_address == stakeholders[s]) return (true, s);
-       }
-       return (false, 0);
-   }
-
-   /**
-    * @notice A method to add a stakeholder.
-    */
-   function addStakeholder() public {
-       address _stakeholder = msg.sender;
-       (bool _isStakeholder, ) = isStakeholder(_stakeholder);
-       if(!_isStakeholder) stakeholders.push(_stakeholder);
-   }
-
-   /**
-    * @notice A method to remove a stakeholder.
-    * @param _stakeholder The stakeholder to remove.
-    */
-   function removeStakeholder(address _stakeholder) public {
-       (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
-       if(_isStakeholder){
-           stakeholders[s] = stakeholders[stakeholders.length - 1];
-           stakeholders.pop();
-       }
-   }
-
-   /**
-    * @notice A method to retrieve the stake for a stakeholder.
-    * @param _stakeholder The stakeholder to retrieve the stake for.
-    * @return uint256 The amount of wei staked.
-    */
-   function stakeOf(address _stakeholder)
-       public
-       view
-       returns(uint256)
-   {
-       return stakes[_stakeholder];
-   }
-
-   /**
-    * @notice A method to the aggregated stakes from all stakeholders.
-    * @return uint256 The aggregated stakes from all stakeholders.
-    */
-   function totalStakes()
-       public
-       view
-       returns(uint256)
-   {
-       uint256 _totalStakes = 0;
-       for (uint256 s = 0; s < stakeholders.length; s += 1){
-           _totalStakes = _totalStakes.add(stakes[stakeholders[s]]);
-       }
-       return _totalStakes;
-   }
-
-   /**
-    * @notice A method for a stakeholder to create a stake.
-    * @param _stake The size of the stake to be created.
-    */
-   function createStake(uint256 _stake)
-       public
-   {
-       if (stakeToken.allowance(msg.sender, address(this)) < _stake) stakeToken.approve(address(this), _stake);
-       stakeToken.burn(_stake);
-       if (stakes[msg.sender] == 0) addStakeholder();
-       stakes[msg.sender] = stakes[msg.sender].add(_stake);
-   }
-
-   /**
-    * @notice A method for a stakeholder to remove a stake.
-    * @param _stake The size of the stake to be removed.
-    */
-   function removeStake(uint256 _stake)
-       public
-   {
-       stakes[msg.sender] = stakes[msg.sender].sub(_stake);
-       if(stakes[msg.sender] == 0) removeStakeholder(msg.sender);
-       stakeToken.mint(msg.sender, _stake);
-   }
-  
-   /**
-    * @notice A method to allow a stakeholder to check his rewards.
-    * @param _stakeholder The stakeholder to check rewards for.
-    */
-   function rewardOf(address _stakeholder)
-       public
-       view
-       returns(uint256)
-   {
-       return rewards[_stakeholder];
-   }
-
-   /**
-    * @notice A method to the aggregated rewards from all stakeholders.
-    * @return uint256 The aggregated rewards from all stakeholders.
-    */
-   function totalRewards()
-       public
-       view
-       returns(uint256)
-   {
-       uint256 _totalRewards = 0;
-       for (uint256 s = 0; s < stakeholders.length; s += 1){
-           _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
-       }
-       return _totalRewards;
-   }
-   
-   /**
-    * @notice A simple method that calculates the rewards for each stakeholder.
-    * @param _stakeholder The stakeholder to calculate rewards for.
-    */
-   function calculateReward(address _stakeholder)
-       public
-       view
-       returns(uint256)
-   {
-       return stakes[_stakeholder] / 100;
-   }
-
-   /**
-    * @notice A method to distribute rewards to all stakeholders.
-    */
-   function distributeRewards()
-       public
-       onlyOwner
-   {
-       for (uint256 s = 0; s < stakeholders.length; s += 1){
-           address stakeholder = stakeholders[s];
-           uint256 reward = calculateReward(stakeholder);
-           rewards[stakeholder] = rewards[stakeholder].add(reward);
-       }
-   }
-
-   /**
-    * @notice A method to allow a stakeholder to withdraw his rewards.
-    */
-   function withdrawReward()
-       public
-   {
-       uint256 reward = rewards[msg.sender];
-       rewards[msg.sender] = 0;
-       stakeToken.mint(msg.sender, reward);
-   }
 }
