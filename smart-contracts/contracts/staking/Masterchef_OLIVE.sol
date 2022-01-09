dfd005e51a7215f68fe5a601895f9ede739d7f97
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-01-09 18:43:47 +0100

    Add MasterChef_CENT.sol

---
+++ smart-contracts/contracts/staking/Masterchef_OLIVE.sol
@@ -0,0 +1,217 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./BEP20/interfaces/IBEP20.sol";
+import "./BEP20/lib/SafeBEP20.sol";
+import "./ERC20/testERC20.sol";
+
+
+// MasterChef is the master of erc20. He can make erc20 and he is a fair guy.
+//
+// Note that it's ownable and the owner wields tremendous power. The ownership
+// will be transferred to a governance smart contract once erc20 is sufficiently
+// distributed and the community can show to govern itself.
+//
+// Have fun reading it. Hopefully it's bug-free. God bless.
+
+contract MasterChef is Ownable, ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount;         // How many LP tokens the user has provided.
+        uint256 rewardDebt;     // Reward debt. See explanation below.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. OLIVE to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that OLIVE distribution occurs.
+        uint256 accOlivePerShare;   // Accumulated OLIVE per share, times 1e12. See below.
+    }
+
+    OliveToken public olive;
+    address public devaddr;
+    uint256 public olivePerBlock;
+    uint256 public constant BONUS_MULTIPLIER = 1;
+
+    PoolInfo[] public poolInfo;
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    uint256 public totalAllocPoint = 0;
+    uint256 public startBlock;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event SetDevAddress(address indexed user, address indexed newAddress);
+    event UpdateEmissionRate(address indexed user, uint256 olivePerBlock);
+
+    constructor(
+        OliveToken _olive,
+        address _devaddr,
+        uint256 _olivePerBlock,
+        uint256 _startBlock
+    ) public {
+        olive = _olive;
+        devaddr = _devaddr;
+        olivePerBlock = _olivePerBlock;
+        startBlock = _startBlock;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    mapping(IERC20 => bool) public poolExistence;
+    modifier nonDuplicated(IERC20 _lpToken) {
+        require(poolExistence[_lpToken] == false, "nonDuplicated: duplicated");
+        _;
+    }
+
+    modifier poolExists(uint256 pid) {
+        require(pid < poolInfo.length, "pool inexistent");
+        _;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner nonDuplicated(_lpToken) {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolExistence[_lpToken] = true;
+        poolInfo.push(PoolInfo({
+        lpToken : _lpToken,
+        allocPoint : _allocPoint,
+        lastRewardBlock : lastRewardBlock,
+        accOlivePerShare : 0
+        }));
+    }
+
+    // Update the given pool's OLIVE allocation point and deposit fee. Can only be called by the owner.
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner poolExists(_pid) {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        return _to.sub(_from).mul(BONUS_MULTIPLIER);
+    }
+
+    // View function to see pending OLIVEs on frontend.
+    function pendingOlive(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accOlivePerShare = pool.accOlivePerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 oliveReward = multiplier.mul(olivePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accOlivePerShare = accOlivePerShare.add(oliveReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accOlivePerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0 || pool.allocPoint == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 oliveReward = multiplier.mul(olivePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        olive.mint(devaddr, oliveReward.mul(888).div(10000));
+        olive.mint(address(this), oliveReward);
+        pool.accOlivePerShare = pool.accOlivePerShare.add(oliveReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to MasterChef for OLIVE allocation.
+    function deposit(uint256 _pid, uint256 _amount) public nonReentrant poolExists(_pid) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accOlivePerShare).div(1e12).sub(user.rewardDebt);
+            if (pending > 0) {
+                safeOliveTransfer(msg.sender, pending);
+            }
+        }
+        if (_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accOlivePerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256 _pid, uint256 _amount) public nonReentrant poolExists(_pid) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accOlivePerShare).div(1e12).sub(user.rewardDebt);
+        if (pending > 0) {
+            safeOliveTransfer(msg.sender, pending);
+        }
+        if (_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accOlivePerShare).div(1e12);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public nonReentrant poolExists(_pid) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        uint256 amount = user.amount;
+        user.amount = 0;
+        user.rewardDebt = 0;
+        pool.lpToken.safeTransfer(address(msg.sender), amount);
+        emit EmergencyWithdraw(msg.sender, _pid, amount);
+    }
+
+    // Safe OLIVE transfer function, just in case if rounding error causes pool to not have enough OLIVEs.
+    function safeOliveTransfer(address _to, uint256 _amount) internal {
+        uint256 oliveBal = olive.balanceOf(address(this));
+        bool transferSuccess = false;
+        if (_amount > oliveBal) {
+            transferSuccess = olive.transfer(_to, oliveBal);
+        } else {
+            transferSuccess = olive.transfer(_to, _amount);
+        }
+        require(transferSuccess, "safeOliveTransfer: transfer failed");
+    }
+
+    // Update dev address by the previous dev.
+    function dev(address _devaddr) public {
+        require(msg.sender == devaddr, "dev: wat?");
+        devaddr = _devaddr;
+        emit SetDevAddress(msg.sender, _devaddr);
+    }
+}
