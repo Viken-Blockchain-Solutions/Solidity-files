929b26bf035f1693085bee8a3704c09616484be0
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-01-10 23:42:12 +0100

    chore: custom masterchef

--- smart-contracts/contracts/staking/MasterChef_CENT.sol
+++ smart-contracts/contracts/staking/MasterChef_CENT.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./SafeERC20.sol";
-import "./testERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../ERC20/testERC20.sol";
 
 
 
