c8227988a1be1ad5b3ed9c0eadb85640fd589829
Ivo Garofalo <ivo.garofalo@gmail.com>
2022-01-18 16:31:52 +0100

    Feat: SingleStaking feature deployed to Ropsten

--- smart-contracts/contracts/README.md
+++ smart-contracts/contracts/README.md
@@ -38,7 +38,7 @@ https://rinkeby.etherscan.io/address/0x9dCf9350505c8f4AfB00AB00740A23970e75E5FD#
     |       Address dev     :                          0x6d300A0e146143f68F318ED045B80b06B0001639
     |
     |    Contract deployed  :
-    |       SingleStaking   :                          0xA59d84a05Da7b71540a438eFc1391F785b4cC80b
+    |       SingleStaking   :                          0xBE53fAf7E1c39c6D6ce0F528328433f6B302fA88,
     |
     |       testERC20       :                          0x12109Fc5e292533D775A17AFb543fdeF15c86f30
     |
--- smart-contracts/scripts/1_deploy_to_rinkeby.js
+++ smart-contracts/scripts/1_deploy_to_rinkeby.js
@@ -1,30 +1,32 @@
 require("dotenv/config");
 
 async function main() {
-    const [deployer]  = await ethers.getSigners();
-    console.log("Deploying contracts with the account:", deployer.address);
-
-    const dev = `${process.env.DEV}`;
     
-    const Staking = await ethers.getContractFactory("SingleStaking");
-    const Token = await ethers.getContractFactory("testERC20");
-    
-    const staking = await Staking.deploy(dev, 1000000000000000000n);
-    const token = await Token.deploy(1000000000000000000000000000n);
+  const [deployer]  = await ethers.getSigners();
 
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const dev = `${process.env.DEV}`;
   
-    console.log(`
-        ----------------------------------------------------------------------------------
-        |    Deployment Status  :                                                          
-        |       Contract owner  :                          ${deployer.address},
-        |       Address dev     :                          ${dev},
-        |
-        |    Contract deployed  :
-        |       SingleStaking   :                          ${staking.address}
-        |       testERC20       :                          ${token.address} 
-        |
-        ----------------------------------------------------------------------------------
-    `);
+  const Staking = await ethers.getContractFactory("SingleStaking");
+  const Token = await ethers.getContractFactory("testERC20");
+  
+  const staking = await Staking.deploy(dev, 1000000000000000000n);
+  const token = await Token.deploy(1000000000000000000000000000n);
+
+
+  console.log(`
+      ----------------------------------------------------------------------------------
+      |    Deployment Status  :                                                          
+      |       Contract owner  :                          ${deployer.address},
+      |       Address dev     :                          ${dev},
+      |
+      |    Contract deployed  :
+      |       SingleStaking   :                          ${staking.address}
+      |       testERC20       :                          ${token.address} 
+      |
+      ----------------------------------------------------------------------------------
+  `);
 }
   
 main()
