34f3cb6e40f800ebecd56a1ccdb342ad2c4b3c11
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-01-22 12:21:45 +0100

    feat: mappings

---
+++ templates/example-files/modifiers.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/** 
+ * @notice This contract contains a set of 'Nice to have" modifers.
+ * @notice onlyAdmin.
+ * @notice notContract.
+ * @notice 
+ */
+
+
+contract Modifers is Context, Ownable {
+
+    address public admin;
+    bool public initialized;
+
+    // errors
+    error OnlyAdmin();
+    error ContractsNotAllowed();
+    error ProxyNotAllowed();
+    error AlreadyInitialized();
+
+
+    /**
+     * @notice Checks if the msg.sender is the admin address.
+     */
+    modifier onlyAdmin() {
+        if (_msgSender() == admin)
+            revert OnlyAdmin();
+        _;
+    }
+
+    /**
+     * @notice Checks if the msg.sender is a contract or a proxy.
+     * @dev requires the method _isContract(address sender).
+     */
+    modifier notContract() {
+        if (_isContract(_msgSender())) revert ContractsNotAllowed();
+        if (_msgSender() == tx.origin) revert ProxyNotAllowed();
+        _;
+    }
+
+    /**
+     * @notice Checks if the called method is already initialzed.
+     * @dev requires the method _isContract(address sender).
+     */
+    modifier notInitialized() {
+        if (initialized) 
+            revert AlreadyInitialized();
+        _;
+        initialized = true;
+    }
+
+     /**
+     * @notice Checks if address is a contract.
+     * @dev It prevents contract from being targeted.
+     * @dev Is required to use the notContract() modifier. 
+     */
+    function _isContract(address addr) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(addr)
+        }
+        return size > 0;
+    }
+}
