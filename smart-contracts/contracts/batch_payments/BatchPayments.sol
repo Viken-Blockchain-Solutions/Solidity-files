a470e6edb2f502b4a36f095dae2f6ed2ada859d6
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-02-17 00:11:57 +0100

    add

--- smart-contracts/contracts/batch_payments/BatchPayments.sol
+++ smart-contracts/contracts/batch_payments/BatchPayments.sol
@@ -1,28 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
 contract BatchPayments {
-
     error Reverted();
+
+    event EthTransfer(address indexed Payer);
     
     function recieve() external payable {
         revert Reverted();
     }
-
+    // 57620 gas
     function batchEtherPayment(address[] calldata recipients, uint256[] calldata values) external payable {
         for (uint256 i = 0; i < recipients.length; i++)
             payable(recipients[i]).transfer(values[i]);
         uint256 balance = address(this).balance;
         if (balance > 0)
             payable(msg.sender).transfer(balance);
+        
+        emit EthTransfer(msg.sender);
     }
 
 
-    /// @notice Costs 77093 gas to tranfer 6e18 to three address.  costs 7351 gas less to execute.
-    function batchERC20Payment(IERC20 token, address[] calldata recipients, uint256[] calldata values) external {
+    /// @notice Costs 71365 gas to tranfer total 6e18 to three address.  costs 7351 gas less to execute.
+    function batchERC20Payment(
+        IERC20 token, 
+        address[] calldata recipients, 
+        uint256[] calldata values
+    ) external {
         for (uint256 i = 0; i < recipients.length; i++)
             token.transferFrom(msg.sender, recipients[i], values[i]);
     }
