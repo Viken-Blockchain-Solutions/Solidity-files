984f625fb3dcc8c117fa1e4fa32962d60a1f072e
Ivo Garofalo <ivo.garofalo@gmail.com>
2022-05-15 16:30:35 +0200

    tests: add unit-tests for Spread.sol

--- smart-contracts/test/5_Spread.test.js
+++ smart-contracts/test/5_Spread.test.js
@@ -1,8 +1,9 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const { parseEther } = require("ethers/lib/utils");
+
     
-describe("Spread_dApp", function () {
+describe.only("Spread_dApp", function () {
     
     let owner, spender1, spender2, receiver1, receiver2, receiver3;
     let erc20_spender1, erc20_spender2, erc20;
@@ -31,7 +32,7 @@ describe("Spread_dApp", function () {
         erc20_spender2 = erc20.connect(spender2);
 
         await erc20_owner.mint(spender1.address, ten_Ether);
-        await erc20_owner.mint(spender1.address, ten_Ether);
+        await erc20_owner.mint(spender2.address, ten_Ether);
         
         before1 = await erc20_spender1.balanceOf(spender1.address);
         before2 = await erc20_spender2.balanceOf(spender2.address);
@@ -45,7 +46,7 @@ describe("Spread_dApp", function () {
 
     describe('Admin features', function () {
         
-        it("should print the related address for these tests", async function () {
+        it("should print the related address for these tests.", async function () {
             console.log(`
                 Test accounts:
                     Spread Contract   :       ${spread.address},
@@ -55,13 +56,12 @@ describe("Spread_dApp", function () {
                     receiver1 Account :       ${receiver1.address},
                     receiver2 Account :       ${receiver2.address},
                     receiver3 Account :       ${receiver3.address}
-            `)
+            `);
         }); 
-    })  
+    });
 
-    describe('Spread assets', function () {
-        
-        it("should execute a batch transfer of Ether to three accounts and check balances after", async function () {
+    describe('Spread - Happy path', function () {
+        it("should execute a batch transfer of Ether to three accounts and check balances after.", async function () {
           
             let tx = await spread.connect(spender1).spreadAsset(
                 [ receiver1.address, receiver2.address, receiver3.address ],
@@ -76,7 +76,7 @@ describe("Spread_dApp", function () {
         
         });
         
-        it("Should emit an event after a ERC20 batch transfer", async function () {
+        it("Should emit an event after calling the spreadERC20 method.", async function () {
             await erc20_spender1.approve(spread.address, five_Ether);
 
             let tx = await spread.connect(spender1).spreadERC20(
@@ -86,7 +86,51 @@ describe("Spread_dApp", function () {
             );
 
             expect(tx).to.emit(erc20, 'Transfer');
-        }); 
+        });
 
-    })
+        it("Should emit an event after calling the spreadERC20Simple method.", async function () {
+            await erc20_spender2.approve(spread.address, five_Ether);
+
+            let tx = await spread.connect(spender2).spreadERC20Simple(
+                erc20.address,
+                [receiver1.address, receiver2.address, receiver3.address], 
+                [amount_one, amount_three, amount_one]
+            );
+
+            expect(tx).to.emit(erc20, 'Transfer');
+        }); 
+    });
+
+    describe('Spread - Fail path', async function () {
+        beforeEach(async function () {
+            spread_spender1 = await spread.connect(spender1);
+            spread_spender2 = await spread.connect(spender2);
+        });
+        it('should revert if the address and values is missing when calling the spreadAsset method.', async function () {
+            await expect(spread_spender1.spreadAsset([], [], {value: ethers.constants.Zero}))
+                .to.be.reverted;
+        });
+        it('should revert if one of the address or values is missing when calling the spreadAsset method.', async function () {
+            await expect(spread_spender1.spreadAsset([receiver1.address], [], {value: amount_five}))
+                .to.be.reverted;
+        });
+        it('should revert if the ether value is to low', async function () {
+            await expect(spread_spender1.spreadAsset([receiver1.address], [amount_six], {value: amount_five}))
+                .to.be.reverted;
+        });
+        it('should revert if the approval is missing before the erc20 tansaction', async function () {
+            await expect(spread_spender2.spreadERC20Simple(erc20.address, [receiver1.address], [amount_six]))
+                .to.be.reverted;
+        });
+        it('should revert if one of the address or values is missing when calling the spreadAsset method.', async function () {
+            await erc20_spender2.approve(spread.address, amount_five);
+            await expect(spread_spender2.spreadERC20(erc20.address, [], [amount_five]))
+                .to.be.reverted;
+        });
+        it('should revert if the user tries to pass zero arrays.', async function () {
+            await erc20_spender2.approve(spread.address, amount_five);
+            await expect(spread_spender2.spreadERC20(erc20, [], []))
+                .to.be.reverted;
+       });
+    });
 });    
