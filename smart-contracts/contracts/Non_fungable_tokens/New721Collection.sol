c98d534f458bb46be03b2659b9a69dca744c7a9f
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-06-03 18:48:46 +0200

    chore: add New721Collection.sol

---
+++ smart-contracts/contracts/Non_fungable_tokens/New721Collection.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/**
+ * @title  New721aCollection.
+ * @author @Dadogg80 for Viken Blockchain Solutions. 
+ *
+ * @notice This smart contract is a generic type contract to create Artist Collections 
+ *         based on the ERC721A improved implementation { developed by chiru-labs } of the IERC721 standard that 
+ *         supports minting multiple tokens for close to the cost of one.
+ *
+ * @dev    This contract contain custom methods and some improvments like:
+ *         - Custom errors for error handling.
+ *         - Supports ERC2981 - Royalty Standard.
+ *         - Supports OpenSea by implementing { contractURI } method for handeling Royalties. 
+ */
+/// @custom:security-contact security@vikenblockchain.com
+contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("NAME", "TICKER") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "INSERT_URI_HERE";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
