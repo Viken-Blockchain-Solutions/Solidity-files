755f56066230ae01d7c7852ab1f713abdbf27709
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-06-04 11:07:52 +0200

    ERC721 Fixes #6

--- smart-contracts/contracts/Non_fungable_tokens/New721Collection.sol
+++ smart-contracts/contracts/Non_fungable_tokens/New721Collection.sol
@@ -10,39 +10,62 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /**
- * @title  New721aCollection.
+ * @title  New721Collection.
  * @author @Dadogg80 for Viken Blockchain Solutions. 
  *
- * @notice This smart contract is a generic type contract to create Artist Collections 
- *         based on the ERC721A improved implementation { developed by chiru-labs } of the IERC721 standard that 
- *         supports minting multiple tokens for close to the cost of one.
- *
+ * @notice This smart contract is a generic type contract to create Artist Collections,
+ *         and list the collection on OpenSea.
  * @dev    This contract contain custom methods and some improvments like:
- *         - Custom errors for error handling.
- *         - Supports ERC721Royalty - Royalty Standard.
+ *         - Supports ERC721Royalty , built on ERC2981 Royalty Standard.
  *         - Supports OpenSea by implementing { contractURI } method for handeling Royalties. 
  */
 /// @custom:security-contact security@vikenblockchain.com
-contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, ERC721Royalty, Ownable {
+contract New721Collection is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, ERC721Royalty, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    string public contractURI = "https://ifwsu1awnie4.usemoralis.com/info.json";
     
-    constructor(string memory name, string memory ticker) ERC721(name, ticker) {}
+    uint mintingFee = 0.5 ether;
+
+    error LowAmount(uint required, uint sentValue);
+
+    event DefaultRoyalty(address indexed royaltyReceiver, uint96 feeNumerator);
+    event Mint(address indexed to, uint indexed tokenId, string uri);
+
+    modifier costs {
+        if (_msgSender() != super.owner()) {
+            if (msg.value < mintingFee) {
+                revert LowAmount({
+                    required: mintingFee,
+                    sentValue: msg.value
+                });
+            }
+        }
+        _;
+    }
+    
+    constructor(address royaltyReceiver, uint96 feeNumerator) ERC721("Test_Collection", "TEST_COLLECTION") {
+        _setDefaultRoyalty(royaltyReceiver, feeNumerator);
+        emit DefaultRoyalty(royaltyReceiver, feeNumerator);
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "https://ifwsu1awnie4.usemoralis.com/info.json";
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://ifwsu1awnie4.usemoralis.com/json/";
     }
 
-    function mint(address to, string memory uri) public onlyOwner {
+    function mint(string memory uri) public payable costs {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
 
-    // The following functions are overrides required by Solidity.
+        _safeMint(_msgSender(), tokenId);
+        _setTokenURI(tokenId, uri);
+
+        emit Mint(_msgSender(), tokenId, uri);
+    }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
@@ -51,7 +74,7 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, 
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage, ERC721Royalty) {
         super._burn(tokenId);
     }
 
@@ -71,5 +94,10 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, 
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function withdraw() external onlyOwner returns(bool) {
+        uint256 balance = address(this).balance;
+        payable(address(owner())).transfer(balance);
     }
 }
