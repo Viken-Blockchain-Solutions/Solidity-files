9014ea3e13050d0aea387816ad649c627287be7e
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-06-12 12:18:42 +0200

    tests: add unit tests for New721Collection.sol

--- smart-contracts/test/6_New_721_collection.test.js
+++ smart-contracts/test/6_New_721_collection.test.js
@@ -1,15 +1,16 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const { parseEther } = require("ethers/lib/utils");
-const { getContractFactory } = require("@nomiclabs/hardhat-ethers/types");
 
     
 describe.only("New721Collection", function () {
     const name = "test bunnies";
     const ticker = "TEST_BUNNIES";
     const feeNumerator = 1000;
+    const contractURI = "https://ifwsu1awnie4.usemoralis.com/info.json";
+    const salesPrice = parseEther("1");
     
-    let ownerConnected, sellerConnected, Contract, contract;
+    let ownerConnected, sellerConnected, contract, Contract;
 
     beforeEach(async () => {
 
@@ -34,12 +35,21 @@ describe.only("New721Collection", function () {
         it('should deploy the contract with the correct ticker', async () => {
             expect(await ownerConnected.symbol()).to.be.equal(ticker);
         });
+        it('should return the contractURI', async () => {
+            expect(await ownerConnected.contractURI()).to.be.equal(contractURI);
+        });
     });
     describe('Minting - Happy path', function () {
         it('should allow the owner to mint', async () => {
             expect(await ownerConnected.mint("00.json"))
                 .to.emit(contract, "Mint")
                     .withArgs(owner.address, 0, "00.json");
+        });
+        it('should return the correct royalty info', async () => {
+            await ownerConnected.mint("00.json");
+            const royaltyInfo = await contract.royaltyInfo(0, salesPrice);
+            expect(await royaltyInfo[0]).to.be.equal(royalty.address);
+            expect(await royaltyInfo[1].toString()).to.be.equal("100000000000000000");
         });
     });
 
